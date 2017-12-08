7952da319d64a1b3dd3c615ed3086a7c1d8d6b61
Kris <christellelocque@hotmail.fr>
2017-12-08 10:29:26 +0100

    premier envoi après résolution du problème avec bonus

--- app.js
+++ app.js
@@ -1,5 +1,18 @@
-$(document).ready(function(){
-	//votre code
+function gateau() {
+$(document).ready(function(){ 
+		var nbr = 0;
+	$(".cake").click(function(){ //écouter les clicks sur la photo
+		var total = ++nbr;
+		$(".counter").html(total); //afficher le nbr de click à la place du 0 
+		
+		if (total%20 == 0) { //si le nombre de click est un multiple de 20
+			var message = ['Attention!', 'Tu vas finir idiot', 'Changes un peu!', 'Ca suffit'];//tableau de message
+			var number = Math.floor ( Math.random() * message.length ); //sélectionne de façon aléatoire les mess
+			alert(message[number]);   //affiche un mess d'alerte aléatoire
 
-
+};
 });
+});
+};
+
+gateau()
